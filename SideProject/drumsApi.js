eeab81685122e3762e657201574366e7c05b1178
Unknown <Ofirtadmor@gmail.com>
2018-01-14 00:11:59 +0200

    configured the loop and changed json accordingly

--- SideProject/drumsApi.js
+++ SideProject/drumsApi.js
@@ -1,5 +1,8 @@
 
 
+var stopLoop = function(){
+Tone.Transport.stop();
+}
 
 var setBeatTime = function (beatObject, snare, kick, hihat) {
   snare.toMaster();
@@ -9,23 +12,36 @@ var setBeatTime = function (beatObject, snare, kick, hihat) {
   hihat.toMaster();
   hihat.retrigger = true;
   hihat.volume.value=-8;
-  Tone.Transport.scheduleRepeat(function (time) {
-    //  kick.trigger(time)
-    kick.start(time,0.001);
-  }
-    , beatObject.kick.repeatMeasure, beatObject.kick.startingMeasure);
 
-  Tone.Transport.scheduleRepeat(function (time) {
-    snare.start(time,0.01)
-    //   snare.trigger(time); 
-  }
-    , beatObject.snare.repeatMeasure, beatObject.snare.startingMeasure);
+  kickLoop = new Tone.Part(function(time){
+  	kick.start(time,0.001);
+  	},beatObject.kick.Pattern.map(function(e,i,array){
+	  return array.slice(0,i+1).join(" + ");
+  }));
+  kickLoop.loopEnd = beatObject.kick.LoopEnd;
+  kickLoop.loop=true;
+   
 
-  Tone.Transport.scheduleRepeat(function (time) {
-    hihat.start(time,0.01);
-    //hihat.trigger(time)
-  }
-    , beatObject.hihat.repeatMeasure, beatObject.hihat.startingMeasure);
+  snareLoop = new Tone.Part(function(time){
+  snare.start(time,0.01);
+  }, beatObject.snare.Pattern.map(function(e,i,array){
+	  return array.slice(0,i+1).join(" + ");
+  })
+  );
+  snareLoop.loopEnd = beatObject.snare.LoopEnd;
+  snareLoop.loop=true;
+
+  hihatLoop = new Tone.Part(function(time){
+  hihat.start(time,0.01); 
+  }, beatObject.hihat.Pattern.map(function(e,i,array){
+	  return array.slice(0,i+1).join(" + ");
+  })
+  );
+  hihatLoop.loopEnd = beatObject.hihat.LoopEnd;
+  hihatLoop.loop=true;
+  hihatLoop.start(0);
+  kickLoop.start(0);
+  snareLoop.start(0);
 }
 var setBeatTempo = function (tempo) {
   Tone.Transport.bpm.value = tempo;
@@ -41,7 +57,8 @@ var players = new Tone.Players(
 function(players)
 {
   setBeatTempo(120);
-  setBeatTime(beats[0], players.get('snare'),players.get('kick'), players.get('hihat'));
+  setBeatTime(beats[0], players.get('snare'),players.get('kick'),
+   players.get('hihat'));
 
 });
 };
@@ -56,133 +73,15 @@ $.getJSON("./DrumBeats.json", function (json) {
     $('#pause').click(function () {
       if (window.playing == true) {
         window.playing = false;
-        Tone.Transport.stop();
+        stopLoop();
       }
     });
   
     $('#play').click(function () {
       if (window.playing == false) {
         window.playing = true;
-        Tone.Transport.start();
+     	  Tone.Transport.start('+0.1');
       }
     });
   });
 });
-
-
-
-  //sampleLoader('./Samples/_HH.wav', context, setup);
-
-
-  /*
-function Kick(context) {
-    this.context = context;
-  };
-  
-  
-  Kick.prototype.setup = function() {
-    this.osc = this.context.createOscillator();
-    this.gain = this.context.createGain();
-    this.osc.connect(this.gain);
-    this.gain.connect(this.context.destination)
-  };
-  
-  Kick.prototype.trigger = function(time) {
-    this.setup();
-  
-    this.osc.frequency.setValueAtTime(150, time);
-    this.gain.gain.setValueAtTime(1, time);
-  
-    this.osc.frequency.exponentialRampToValueAtTime(0.01, time + 0.5);
-    this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
-  
-    this.osc.start(time);
-  
-    this.osc.stop(time + 0.5);
-  };
-  
-  function Snare(context) {
-    this.context = context;
-  };
-  
-  Snare.prototype.setup = function() {
-    this.noise = this.context.createBufferSource();
-    this.noise.buffer = this.noiseBuffer();
-  
-    var noiseFilter = this.context.createBiquadFilter();
-    noiseFilter.type = 'highpass';
-    noiseFilter.frequency.value = 1000;
-    this.noise.connect(noiseFilter);
-  
-    this.noiseEnvelope = this.context.createGain();
-    noiseFilter.connect(this.noiseEnvelope);
-  
-    this.noiseEnvelope.connect(this.context.destination);
-  
-    this.osc = this.context.createOscillator();
-    this.osc.type = 'triangle';
-  
-    this.oscEnvelope = this.context.createGain();
-    this.osc.connect(this.oscEnvelope);
-    this.oscEnvelope.connect(this.context.destination);
-  };
-  
-  Snare.prototype.noiseBuffer = function() {
-    var bufferSize = this.context.sampleRate;
-    var buffer = this.context.createBuffer(1, bufferSize, this.context.sampleRate);
-    var output = buffer.getChannelData(0);
-  
-    for (var i = 0; i < bufferSize; i++) {
-      output[i] = Math.random() * 2 - 1;
-    }
-  
-    return buffer;
-  };
-  
-  Snare.prototype.trigger = function(time) {
-    this.setup();
-  
-    this.noiseEnvelope.gain.setValueAtTime(1, time);
-    this.noiseEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.2);
-    this.noise.start(time)
-  
-    this.osc.frequency.setValueAtTime(100, time);
-    this.oscEnvelope.gain.setValueAtTime(0.7, time);
-    this.oscEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.1);
-    this.osc.start(time)
-  
-    this.osc.stop(time + 0.2);
-    this.noise.stop(time + 0.2);
-  };
-  
-  function HiHat(context, buffer) {
-    this.context = context;
-    this.buffer = buffer;
-  };
-  
-  HiHat.prototype.setup = function() {
-    this.source = this.context.createBufferSource();
-    this.source.buffer = this.buffer;
-    this.source.connect(this.context.destination);
-  };
-  
-  HiHat.prototype.trigger = function(time) {
-    this.setup();
-  
-    this.source.start(time);
-  };
-  
-  var sampleLoader = function(url, context, callback) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
-  
-    request.onload = function() {
-      context.decodeAudioData(request.response, function(buffer) {
-        window.buffer = buffer;
-        callback();
-      });
-    };
-  
-    request.send();
-  };*/
